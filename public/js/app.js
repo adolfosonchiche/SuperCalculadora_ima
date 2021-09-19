0405601302ce2dd73c3183cd9bf329c86185774a
adolfosonchiche <sonadolfo36@gmail.com>
2021-09-18 19:38:38 -0600

    ui

--- public/js/app.js
+++ public/js/app.js
@@ -2,14 +2,13 @@ function calcular(){
     let val1 = document.getElementById("num1").value;
     let val2 = document.getElementById("num2").value;
     let operacion = document.getElementById("operador").value;
-    console.log(operacion);
     if(val1 === "" || val2 === ""){
      
-        alert("tienes que ingresar los 2 numeros para operarlos " );
+        console.log("tienes que ingresar los 2 numeros para operarlos " );
         console.log('hola mundo');
     }else{
       console.log('hola mundo');
-      alert("valor uno = " + val1 + "\n valor dos = " + val2);
+      console.log("valor uno = " + val1 + "\n valor dos = " + val2);
     }
-    document.write('<h1>'+ operacion+' </h1>')
+    document.getElementById('cont3').innerHTML= '<h3>'+ operacion+' </h3>';
 }
